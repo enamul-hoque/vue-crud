1069813b7e5587906d227e099b95c196eb167b61
enamul-hoque <enamul.hoque.mohon@gmail.com>
2020-09-29 01:58:23 +0600

    improve:  hidden columns

--- src/components/ListTable.vue
+++ src/components/ListTable.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="list-table">
-    <ejs-grid :dataSource="listData" :toolbar='toolbarOptions'>
-		<e-columns>
-			<e-column :field='key' :headerText='item.title' v-for="(item, key, indx) in columnsTitles" :key="indx" v-if="item.hidden === false"></e-column>
-		</e-columns>
-	</ejs-grid>
+    <ejs-grid :dataSource="listData" :toolbar="toolbarOptions">
+        <e-columns>
+            <e-column :field="key" :headerText="item.title" v-for="(item, key, indx) in columnsTitles" :key="indx"></e-column>
+        </e-columns>
+    </ejs-grid>
   </div>
 </template>
 
@@ -12,29 +12,42 @@
 import { Toolbar, Search } from '@syncfusion/ej2-vue-grids';
 
 export default {
-	name: "ListTable",
-	data() {
-		return {
-			listData: [],
-			toolbarOptions: ['Search'],
-			columnsTitles: {}
-		}
-	},
-	beforeCreate() {
-		fetch('./api/list.php')
-			.then(res => res.json())
-			.then(data => {
-				this.columnsTitles = data.data.headers[0];
-				console.log( data.data.headers[0] );
-				console.log( data.data.rows );
-				this.listData = data.data.rows;
-			})
-			.catch(error => {
-				console.log( error );
-			});
-	},
-	provide: {
-		grid: [Toolbar, Search]
-	}
+    name: "ListTable",
+    data() {
+        return {
+            listData: [],
+            columnsData: {},
+            toolbarOptions: ["Search"]
+        }
+    },
+    beforeCreate() {
+        fetch("./api/list.php")
+            .then(res => res.json())
+            .then(data => {
+                this.columnsData = data.data;
+                this.listData = data.data.rows;
+            })
+            .catch(error => {
+                console.log( error );
+            });
+    },
+    provide: {
+        grid: [Toolbar, Search]
+    },
+    computed: {
+        columnsTitles() {
+            if ( !('headers' in this.columnsData) ) return {};
+
+            let newObj = {};
+
+            for (const columnName in this.columnsData.headers[0]) {
+                if ( !this.columnsData.headers[0][ columnName ].hidden ) {
+                    newObj[columnName] = !this.columnsData.headers[0][ columnName ];
+                }
+            }
+
+            return newObj;
+        }
+    }
 };
 </script>
