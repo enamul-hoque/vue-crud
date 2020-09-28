278b4a8ee336c46a128306eb3391b91bbda54b5f
enamul-hoque <enamul.hoque.mohon@gmail.com>
2020-09-28 14:24:59 +0600

    merged:  bundled route js files

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vue CRUD</title>
+    <link rel="icon" href="dist/favicon.ico">
     <link rel="stylesheet" href="https://cdn.syncfusion.com/ej2/material.css">
-    <link rel="stylesheet" href="http://localhost:8080/css/app.css">
+    <link rel="stylesheet" href="dist/css/app.css">
 </head>
 <body>
     <div id="app"></div>
     
-    <script src="http://localhost:8080/js/app.js"></script>
-    <script src="http://localhost:8080/js/about.js"></script>
+    <script src="dist/js/app.js"></script>
 </body>
 </html>
