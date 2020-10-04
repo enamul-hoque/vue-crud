f9c39e29e6b99ca02821e8958f7b7ed80aa66bdb
mohon-xs <mohon@xpeedstudio.com>
2020-10-04 14:18:02 +0600

    update

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vue CRUD</title>
     <link rel="icon" href="dist/favicon.ico">
-    <link rel="stylesheet" href="https://cdn.syncfusion.com/ej2/material.css">
+    <link rel="stylesheet" href="dist/assets/css/theme.css">
     <link rel="stylesheet" href="dist/css/app.css">
 </head>
 <body>
